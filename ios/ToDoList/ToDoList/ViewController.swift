7c38a68c07181a60fc4abadcec45ae4981ce151e
limwin94 <amuguna94@gmail.com>
2020-04-06 20:00:43 +0900

    [#2] chore: 프로젝트 생성

---
+++ ios/ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by 임승혁 on 2020/04/06.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
