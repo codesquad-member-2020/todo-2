cb7917a403978cb7d4114c98ce62506d5e240b5f
blossun <dnsngu@gmail.com>
2020-04-07 23:29:49 +0900

    [#8] 프로젝트 초기화
    - 스프링 프로젝트 생성
    - gitignore 설정
    - 로깅설정

---
+++ be/src/test/java/com/codesquad/todo2/Todo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.todo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
