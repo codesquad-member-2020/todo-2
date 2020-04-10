64a76e033a92cd778e5f36de20ddf0344d0594f5
Lime-Lee <prestoferoce@gmail.com>
2020-04-10 14:26:17 +0900

    [#15] fix: 웹팩 설정 수정

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/js/index.js',
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
