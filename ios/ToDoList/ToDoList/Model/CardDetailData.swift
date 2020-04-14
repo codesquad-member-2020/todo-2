7be94352d65451c6ae8ae4bf1467ec1f46ad7ae0
limwin94 <amuguna94@gmail.com>
2020-04-14 16:54:43 +0900

    [#22] feat: 새 카드 추가 기능 작성

--- ios/ToDoList/ToDoList/Model/CardDetailData.swift
+++ ios/ToDoList/ToDoList/Model/CardDetailData.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-// 카드 디테일 뷰를 위한 모델
 struct CardDetailData: Codable {
     var cardId: Int
     var user_name: String
--- ios/ToDoList/ToDoList/View/TodoListViewController.swift
+++ ios/ToDoList/ToDoList/View/TodoListViewController.swift
@@ -15,7 +15,13 @@ class TodoListViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var columnName: UILabel!
     
     @IBAction func addNewCardButton(_ sender: Any) {
+        guard let addNewCardView = self.storyboard?.instantiateViewController(withIdentifier: "TaskDetailViewController") as? TaskDetailViewController else { return }
+        self.present(addNewCardView, animated: true)
         
+        addNewCardView.createTask = {
+            self.tableViewDataSource.cardList?.append($0)
+            self.taskCardTableView.reloadData()
+        }
     }
     
     let tableViewDataSource = TodoListDataSource()
@@ -29,7 +35,7 @@ class TodoListViewController: UIViewController, UITableViewDelegate {
         taskCardTableView.dataSource = tableViewDataSource
         
         tableViewDataSource.handler = {
-            self.cardCount.text = String(self.tableViewDataSource.cardList!.count)
+            self.cardCount.text = String(self.tableViewDataSource.cardList?.count ?? 0 )
         }
     }
     
@@ -41,7 +47,6 @@ class TodoListViewController: UIViewController, UITableViewDelegate {
         let currentRow = (indexPath?.row)!
         
         detailView.cardDetailData = tableViewDataSource.cardList?[currentRow]
-        detailView.editIndex = currentRow
         
         detailView.editTask = {
             self.tableViewDataSource.cardList?[currentRow] = $0
