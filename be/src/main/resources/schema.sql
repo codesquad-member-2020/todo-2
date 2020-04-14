bbf34007566a949d379fef7fd3b8b5145a5a4a90
David Chung <david215@berkeley.edu>
2020-04-14 22:36:45 +0900

    [#17] feat: DB schema 정의 + 초기 데이터 추가
    - Log를 제외한 테이블 정의.
    - 초기 데이터는 Todo, Doing, Done 세 개의 카테고리를 가지고 시작.

---
+++ be/src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS card;
+
+CREATE TABLE IF NOT EXISTS user
+(
+    id       BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(20),
+    password VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS project
+(
+    id    BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(64)
+);
+
+CREATE TABLE IF NOT EXISTS category
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title       VARCHAR(64),
+    is_deleted  BOOLEAN,
+    project     BIGINT REFERENCES project (id),
+    project_key INT
+);
+
+CREATE TABLE IF NOT EXISTS card
+(
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title        VARCHAR(64),
+    content      VARCHAR(500),
+    time_created DATETIME,
+    is_deleted   BOOLEAN,
+    user         BIGINT REFERENCES user (id),
+    category     BIGINT REFERENCES category (id),
+    category_key INT
+);
+
